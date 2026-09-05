170515537cd5ea1780a466c041a3a6213487aeef
Humayal <humayal.888@gmail.com>
2026-09-04 22:19:00 -0700

    New portfolio

---
+++ jscode.js
@@ -0,0 +1,17 @@
+let name = "Muhammad Humayal";
+console.log(name);
+let age = 16;
+let isStudent = true;
+console.log(age) ;
+console.log(isStudent);
+let myName = "Muhammad Humayal";
+let myAge = 16;
+let myCity = "Faisalabad";
+function introduce(name, city) {
+    return name + " is from " + city;        
+ }
+console.log(introduce(myName, myCity));
+
+
+
+

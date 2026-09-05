16a678a632d6c2c71151ee680e541602a97c37e2
Humayal <humayal.888@gmail.com>
2026-09-05 01:37:52 -0700

    Final cleanup

--- jscode.js
+++ jscode.js
@@ -1,17 +1,8 @@
-let name = "Muhammad Humayal";
-console.log(name);
-let age = 16;
-let isStudent = true;
-console.log(age) ;
-console.log(isStudent);
+// Variable Declarations
 let myName = "Muhammad Humayal";
 let myAge = 16;
 let myCity = "Faisalabad";
-function introduce(name, city) {
-    return name + " is from " + city;        
- }
-console.log(introduce(myName, myCity));
-
-
-
-
+function introduce(name,city) {
+    return name = "is from" + city;
+}
+console.log(introduce(myName,myCity));
